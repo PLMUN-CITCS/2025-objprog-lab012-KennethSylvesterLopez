a059ef2b4a23490b0201ff713c892bfd3350e814
Kenneth Lopez <lopezkennethsylvester_bscs@plmun.edu.ph>
2025-03-19 17:57:47 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,17 @@
+public class MethodDemo {
+    // Main method
+    public static void main(String[] args) {
+        greet(); // Calling the greet method
+        printMessage("This is my message!"); // Calling the printMessage method
+    }
+
+    // Greet method
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+
+    // PrintMessage method
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
